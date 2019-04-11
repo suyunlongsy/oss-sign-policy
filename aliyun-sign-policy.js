de7f60c9d59a74ac5ae61a067920ea2c86506e24
suyunlongsy <suyunlongsy@gmail.com>
2019-04-11 14:16:54 +0800

    Update aliyun-sign-policy.js

--- aliyun-sign-policy.js
+++ aliyun-sign-policy.js
@@ -2,6 +2,15 @@ import CryptoJS from 'crypto-js'
 const Base64 = CryptoJS.enc.Base64
 const Utf8 = CryptoJS.enc.Utf8
 
+const getTimestamp = () => {
+	const date = new Date()
+	const year = date.getFullYear()
+	const month = date.getMonth() + 1
+	const day = date.getDate()
+
+	return `${year}${month < 10 ? '0' + month : month}${day < 10 ? '0' + day : day}`
+}
+
 const getOssPolicyAndSign = () => {
 	const accessid = app.oss.accessKeyId
 	const accesskey = app.oss.accessKeySecret
@@ -14,7 +23,12 @@ const getOssPolicyAndSign = () => {
 	const policyText = {
 		"expiration": srcT, //设置该Policy的失效时间，超过这个失效时间之后，就没有办法通过这个policy上传文件了
 		"conditions": [
-			["content-length-range", 0, 1048576000] // 设置上传文件的大小限制
+			["content-length-range", 0, 1048576000], // 设置上传文件的大小限制
+			[
+				"starts-with",
+				"$key",
+				`${getTimestamp()}/`
+			] // 设置 上传限制的 开头
 		]
 	}
   
@@ -22,10 +36,10 @@ const getOssPolicyAndSign = () => {
 	const message = policyBase64
 
 	const bytes = CryptoJS.HmacSHA1(message, accesskey).toString()
-// 	const bytes = CryptoJS.HmacSHA1(message, accesskey).toString(Utf8)
-	
-// 	const signature = Base64.stringify(Utf8.parse(bytes))
 	const signature = Base64.stringify(Utf8.parse(bytes))
+	
+// 	const bytes = CryptoJS.HmacSHA1(message, accesskey).toString(Utf8)
+// 	const signature = Base64.stringify(bytes)
 
 	return {
 		signature,
